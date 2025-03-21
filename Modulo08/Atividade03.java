4d10c317d556aaec659cabfe5803068892f0f2f0
Rosana Leles <97849886+lelesrosana@users.noreply.github.com>
2025-03-21 11:47:51 -0300

    Removendo atividades antigas e modificando módulos

--- Modulo08/Atividade03.java
+++ Modulo08/Atividade03.java
@@ -3,33 +3,28 @@ package Modulo08;
 import java.util.Scanner;
 
 public class Atividade03 {
-	/* 
-	Tentativa de implementando com uma função para limpar a tela usando recursos da tabela ASCII
-	\33 quando combinado modifica a saída do terminal
-	[H  mover o cursor para o canto superior 
-	[2J para apagar todo o conteúdo
-	public static void limpandoTela() {
-		System.out.print("\033[H\033[2J"); 
-		System.out.flush();  // comando para a limpeza de buffer
+	/*
+	 * Tentativa de implementando com uma função para limpar a tela usando recursos
+	 * da tabela ASCII \33 quando combinado modifica a saída do terminal [H mover o
+	 * cursor para o canto superior [2J para apagar todo o conteúdo public static
+	 * void limpandoTela() { System.out.print("\033[H\033[2J"); System.out.flush();
+	 * // comando para a limpeza de buffer
 	 */
 
-	public static void limpatela() {   //  metodo pesquisado
+	public static void limpatela() { // metodo pesquisado
 		for (int i = 0; i < 50; i++)
 			System.out.println();
 	}
 
-	
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		int age = 0, lessTwentyOne = 0, moreFifty = 0;
 
 		System.out.println("Escolha o valor 1 para inicializar ou -1 para finalizar a contagem: ");
 		int cont = read.nextInt();
-		
 		limpatela();
 
 		if (cont >= 0) {
-
 			System.out.println("\nDigite uma idade: ");
 			age = read.nextInt();
 
@@ -37,21 +32,21 @@ public class Atividade03 {
 				limpatela();
 				if (age < 21) {
 					lessTwentyOne++;
-				} else if( age > 50) {
+				} else if (age > 50) {
 					moreFifty++;
 				}
-				System.out.println("Digite uma idade: ");
+				System.out.println("Digite uma idade ou -1 para finalizar a pesquisa: ");
 				age = read.nextInt();
+				limpatela();
 			}
-			limpatela();
 			System.out.println("\nPesquisa finalizada.");
 
 		} else {
 			System.out.println("Pesquisa finalizada.");
-			
+
 		}
 		System.out.println("\nTotal de pessoas menores de 21 anos: " + lessTwentyOne);
 		System.out.println("\nTotal de pessoas maiores de 50 anos: " + moreFifty);
 	}
-	
+
 }
